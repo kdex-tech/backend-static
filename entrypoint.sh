503107056b1c1a2ffc50c072d5ec0d7272e16517
Raymond Augé <rayauge@doublebite.com>
2026-02-20 15:36:44 -0500

    fix typo
    Signed-off-by: Raymond Augé <rayauge@doublebite.com>

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-// if DUMP_PUBLIC_RESOURCES is set, dump the public resources directory
+# if DUMP_PUBLIC_RESOURCES is set, dump the public resources directory
 if [ ! -z "${DUMP_PUBLIC_RESOURCES}" ]; then
     tree ${PUBLIC_RESOURCES_DIR:-/public}
 fi
